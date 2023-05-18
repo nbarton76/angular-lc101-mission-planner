f53c895008847a7b40800fb5a663aff7906f9cb9
nbarton76 <52866856+nbarton76@users.noreply.github.com>
2023-05-17 21:59:36 -0500

    Update crew.component.ts

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,23 +1,42 @@
-<div class="displayBox">
-    <h3>Equipment</h3>
-    <ol>
-        <li *ngFor="let item of equipmentItems">
-            <button [class.active]="item.mass+cargoMass<=maximumAllowedMass && cargoHold.length < maxItems" [disabled]="item.mass+cargoMass>maximumAllowedMass || cargoHold.length >= maxItems" (click)="nearCapacity = addItem(item)">
-                Add to Cargo Hold
-            </button>
-            {{item.name}}, {{item.mass}} kg
-        </li>
-    </ol>
-</div>
+import { Component, OnInit } from '@angular/core';
 
-<hr>
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
 
-<div class = "displayBox">
-    <h3>Cargo Hold: {{cargoHold.length}}/{{maxItems}} Spots Filled</h3>
-    <ol>
-        <li *ngFor="let equipment of cargoHold">{{equipment.name}}</li>
-    </ol>
-    <p>Mass in Hold: {{cargoMass}} kg</p>
-    <p [class.nearMaxMass]="nearCapacity">Mass Budget Remaining: {{maximumAllowedMass - cargoMass}} kg</p>
-    <button (click)="cargoHold=[]; cargoMass=0; nearCapacity = false">Empty Hold</button>
-</div>
+  inCrew: boolean = false;
+  crew: object[] = [];
+
+  candidates: object[] = [
+    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
+    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
+    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
+    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
+    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
+    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
+    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  addCrewMember(member: object) {
+    if (this.crew.includes(member)) {
+      this.inCrew = true;
+    } else {
+      this.inCrew = false;
+    }
+    
+    if (this.inCrew) {
+        let index = this.crew.indexOf(member);
+        this.crew.splice(index, 1);
+    } else if (!this.inCrew && this.crew.length < 3) {
+        this.crew.push(member);
+    }
+  }
+
+}
